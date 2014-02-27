336b6877da3816f1567f0d40ff11427dbc6862dd
jeasonyoung <77349691@qq.com>
2014-02-27 15:55:11 +0800

    默认消息处理

---
+++ src/main/java/ipower/micromessage/service/http/impl/DefaultEventMessageHandlerImpl.java
@@ -0,0 +1,46 @@
+package ipower.micromessage.service.http.impl;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.apache.log4j.Logger;
+
+import ipower.micromessage.msg.BaseMessage;
+import ipower.micromessage.msg.events.EventMessage;
+import ipower.micromessage.msg.resp.BaseRespMessage;
+import ipower.micromessage.msg.resp.TextRespMessage;
+import ipower.micromessage.service.http.IMessageHandler;
+
+/**
+ * 默认事件消息处理实现。
+ * @author yangyong.
+ * @since 2014-02-27.
+ * */
+public class DefaultEventMessageHandlerImpl implements IMessageHandler {
+	private static Logger logger = Logger.getLogger(DefaultEventMessageHandlerImpl.class);
+	
+	@Override
+	public BaseRespMessage handler(BaseMessage msg) {
+		logger.info("开始默认事件消息处理...");
+		EventMessage event = (EventMessage)msg;
+		if(event == null){
+			logger.error("转换为事件请求消息时失败！");
+		}
+		StringBuilder builder = new StringBuilder();
+		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.SIMPLIFIED_CHINESE);
+		builder.append("尊敬的微信用户:"+ msg.getFromUserName() + ",\r\n")
+			   .append("您刚才发送了["+msg.getMsgType()+"]类型事件消息,\r\n")
+			   .append("当前时间："+ sf.format(new Date()))
+			   .append("感谢您的光顾，我们正在努力的开发中，敬请期待...\r\n");
+		
+		if(event != null){
+			builder.append("事件类型：" + event.getEvent());
+		}
+		TextRespMessage respMessage  = new TextRespMessage();
+		respMessage.setToUserName(msg.getFromUserName());
+		respMessage.setFromUserName(msg.getToUserName());		
+		respMessage.setContent(builder.toString()); 
+		return respMessage;
+	}
+}
---
+++ src/main/java/ipower/micromessage/service/http/impl/DefaultMessageHandlerImpl.java
@@ -0,0 +1,46 @@
+package ipower.micromessage.service.http.impl;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.apache.log4j.Logger;
+
+import ipower.micromessage.msg.BaseMessage;
+import ipower.micromessage.msg.req.BaseReqMessage;
+import ipower.micromessage.msg.resp.BaseRespMessage;
+import ipower.micromessage.msg.resp.TextRespMessage;
+import ipower.micromessage.service.http.IMessageHandler;
+
+/**
+ * 默认消息处理实现。
+ * @author yangyong.
+ * @since 2014-02-27.
+ * */
+public class DefaultMessageHandlerImpl implements IMessageHandler {
+	private static Logger logger = Logger.getLogger(DefaultMessageHandlerImpl.class);
+	
+	@Override
+	public BaseRespMessage handler(BaseMessage msg) {
+		logger.info("开始默认消息处理...");
+		BaseReqMessage req = (BaseReqMessage)msg;
+		if(req == null){
+			logger.error("转换为请求消息时失败！");
+		}
+		StringBuilder builder = new StringBuilder();
+		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.SIMPLIFIED_CHINESE);
+		builder.append("尊敬的微信用户:"+ msg.getFromUserName() + ",\r\n")
+			   .append("您刚才发送了["+msg.getMsgType()+"]类型消息,\r\n")
+			   .append("当前时间："+ sf.format(new Date()))
+			   .append("感谢您的光顾，我们正在努力的开发中，敬请期待...\r\n");
+		
+		if(req != null){
+			builder.append("消息ID：" + req.getMsgId());
+		}
+		TextRespMessage respMessage  = new TextRespMessage();
+		respMessage.setToUserName(msg.getFromUserName());
+		respMessage.setFromUserName(msg.getToUserName());		
+		respMessage.setContent(builder.toString()); 
+		return respMessage;
+	}
+}
