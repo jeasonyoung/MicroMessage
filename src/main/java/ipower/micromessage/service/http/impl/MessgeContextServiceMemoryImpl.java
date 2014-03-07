0d4d8dec48a0aceded63b3ed57b42f50995ba33d
jeasonyoung <77349691@qq.com>
2014-03-07 14:32:33 +0800

    添加上下文内存实现

---
+++ src/main/java/ipower/micromessage/service/http/impl/MessgeContextServiceMemoryImpl.java
@@ -0,0 +1,30 @@
+package ipower.micromessage.service.http.impl;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import ipower.micromessage.msg.MicroContext;
+import ipower.micromessage.service.http.IMessgeContextService;
+
+/**
+ * 消息上下文服务内存模式实现。
+ * @author yangyong.
+ * @since 2014-03-07.
+ * */
+public class MessgeContextServiceMemoryImpl implements IMessgeContextService {
+	private static final Map<String, MicroContext> cache = Collections.synchronizedMap(new HashMap<String, MicroContext>());
+	
+	@Override
+	public synchronized MicroContext get(String openId) {
+		if(openId == null || openId.trim().isEmpty()) return null;
+		return cache.get(openId);
+	}
+
+	@Override
+	public synchronized void Update(MicroContext context) {
+		if(context == null || context.getOpenId() == null || context.getOpenId().trim().isEmpty())
+			return;
+		cache.put(context.getOpenId(), context);
+	}
+}
